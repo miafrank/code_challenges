bad0c88e10921f6cefeb94437ab5819c84ef5503
Mia Frank <31712634+miafrank@users.noreply.github.com>
2024-01-03 17:39:59 -0600

    Refactor majority_element function and update test cases

--- challenges/majority_element.py
+++ challenges/majority_element.py
@@ -1,5 +1,5 @@
 # Given an array nums of size n, return the majority element.
-# The majority element is the element that appears more than ⌊n / 2⌋ times. 
+# The majority element is the element that appears more than ⌊n / 2⌋ times.
 # You may assume that the majority element always exists in the array.
 # Example 1:
 
@@ -10,23 +10,20 @@
 # Input: nums = [2,2,1,1,1,2,2]
 # Output: 2
 
-# Solution: 
+# Solution:
 # Create dictionary where k is the number and v is the amount of times it appears
 # Return the k with the highest value
 
 from typing import List
 
 
-def majority_element(self, nums: List[int]) -> int:
+def majority_element(nums: List[int]) -> int:
     nums_count = {}
     acc = 1
     for num in nums:
-        if num in nums_count: 
+        if num in nums_count:
             num_value = nums_count.get(num)
             nums_count[num] = acc + num_value
         else:
             nums_count[num] = 1
     return max(nums_count, key=lambda k: nums_count.get(k))
-
-print(majority_element(nums = [3,2,3]))
-print(majority_element(nums = [2,2,1,1,1,2,2]))
--- tests/test_majority_element.py
+++ tests/test_majority_element.py
@@ -1,6 +1,6 @@
-import challenges.majority_element as majority_element
+from challenges.majority_element import majority_element
 
 
 def test_majority_element():
-    assert(majority_element(nums = [3,2,3]) == 3)
-    assert(majority_element(nums = [2,2,1,1,1,2,2]) == 2)
+    assert majority_element(nums=[3, 2, 3]) == 3
+    assert majority_element(nums=[2, 2, 1, 1, 1, 2, 2]) == 2
